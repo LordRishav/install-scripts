522cab9b09e55d4754fad446c97836c8895f3207
arch-linux-enthusiast <anonymous9124591@protonmail.com>
2021-06-09 18:33:11 +0530

    new file:   i3-xorg.sh

---
+++ i3-xorg.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#This install script installs i3 WM and related tools. It uses the Xorg protocol.
+
+#Dependencies: git, pacman, a vanilla install of Arch Linux with the base-devel package 
+#Packages which will be installed: xorg-xserver, i3-gaps, i3blocks, paru-bin, maim, pulseaudio,
+#                                  pulseaudio-alsa, pulseaudio-bluetooth, bluez, bluez-utils,
+#                                  fish (will be set to be default shell), ttf-jetbrains-mono,
+#                                  pango, nm-applet, st, dmenu, ranger, mate-calc, chromium, 
+#                                  clipmenu, dunst, redshift, feh, light
+#
+#You will also need my scripts as well
+
+git clone https://github.com/kibybyte/i3-xorg
+
+sudo pacman -S xorg-xserver i3-gaps i3blocks paru-bin maim pulseaudio pulseaudio-alsa pulseaudio-bluetooth bluez bluez-utils fish ttf-jetbrains-mono pango nm-applet st dmenu ranger mate-calc chromium dunst redshift feh light --noconfirm
+
+paru -S clipmenu --noconfirm
